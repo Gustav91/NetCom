1f341a219d0e0952df36312844c907ddb506b8db
Gustav Johnsson <hapaitata@hotmail.com>
2015-06-13 17:40:12 +0200

    First attempt
    Doesn't work yet, message doesn't get through.

--- NetComServer/src/server/Main.java
+++ NetComServer/src/server/Main.java
@@ -1,9 +1,35 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Socket clientSocket = null;
+		ServerSocket serverSocket = null;
+		Scanner sc = null;
+		try{
+			serverSocket = new ServerSocket(6083);
+			System.out.println("Server started....");
+			clientSocket = serverSocket.accept();
+			System.out.println("Client connected.");
+			sc = new Scanner(clientSocket.getInputStream());
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		String message;
+		
+		while(true){
+			if(sc.hasNext()){
+				message = sc.nextLine();
+				System.out.println("Client message: " + message);
+			}
+		}
 
 	}
 
